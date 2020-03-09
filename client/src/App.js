b6bf815497a467acf56d46bdd73823abbb83e72d
Darzamat12 <heavylesha@gmail.com>
2020-03-09 23:15:29 +0300

    fixed some issues and added some tests

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Canvas from './components/canvas';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Canvas from "./components/canvas";
 
 class App extends Component {
   render() {
@@ -11,8 +11,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1>This is my solution to the task</h1>
         </header>
-        <Canvas className='canvas' />
-        <h2>Change input information in respective file in the root of the project to change this drawing</h2>
+        <Canvas className="canvas" />
+        <h2>
+          Change input information in respective file in the root of the project
+          to change this drawing
+        </h2>
       </div>
     );
   }
--- client/src/components/canvas.js
+++ client/src/components/canvas.js
@@ -1,54 +1,74 @@
-import React, { Component } from 'react';
-import './canvas.css'
+import React, { Component } from "react";
+import "./canvas.css";
 
 class Canvas extends Component {
   constructor() {
     super();
     this.state = {
-      data: '',
-      canvasArray:null,
+      data: "",
+      canvasArray: null
     };
   }
 
   componentDidMount() {
-    fetch('/api/canvasdata')
+    fetch("/api/canvasdata")
       .then(res => res.text())
-      .then(data => this.setState({data}, () => console.log('Customers fetched...', data)));
+      .then(data =>
+        this.setState({ data }, () => console.log("Customers fetched...", data))
+      );
   }
 
- 
-
   render() {
-    
-    let array=this.state.data.split(',').join('').split('\n');
+    let array = this.state.data
+      .split(",")
+      .join("")
+      .split("\n");
     return (
-     
       <table cellPadding={0} cellSpacing={0}>
         <tbody>
-        {array.map((el,index)=>{
-         return <tr key={`tr${index}`}>{
-            el.split('').map((element,indexTd)=>{
-              switch(element){
-                case '-': case '|':{
-                  return <td key={`td${indexTd}`} style={{backgroundColor:'black', }}></td>
-              
-                }
-                case ' ':{
-                  return <td key={`td${indexTd}`} style={{backgroundColor:'yellow',}}></td>
-              
-                }
-                case 'X':{
-                  return <td key={`td${indexTd}`} style={{backgroundColor:'blue', }}></td>
-            
-                }
-                default:{
-                  return  <td key={`td${indexTd}`} style={{backgroundColor:'green',}}></td>
-                }
-                
-              }
-            })
-            }</tr>
-        })}
+          {array.map((el, index) => {
+            return (
+              <tr key={`tr${index}`}>
+                {el.split("").map((element, indexTd) => {
+                  switch (element) {
+                    case "-":
+                    case "|": {
+                      return (
+                        <td
+                          key={`td${indexTd}`}
+                          style={{ backgroundColor: "black" }}
+                        ></td>
+                      );
+                    }
+                    case " ": {
+                      return (
+                        <td
+                          key={`td${indexTd}`}
+                          style={{ backgroundColor: "yellow" }}
+                        ></td>
+                      );
+                    }
+                    case "X": {
+                      return (
+                        <td
+                          key={`td${indexTd}`}
+                          style={{ backgroundColor: "blue" }}
+                        ></td>
+                      );
+                    }
+                    default: {
+                      return (
+                        <td
+                          key={`td${indexTd}`}
+                          style={{ backgroundColor: "green" }}
+                        ></td>
+                      );
+                    }
+                  }
+                })}
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     );
